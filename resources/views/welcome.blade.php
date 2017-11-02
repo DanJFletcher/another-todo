7297bb22e6ee18b41e97cd55993b538fe564155a
Dan Fletcher <dannyjfletcher@outlook.com>
2017-11-01 21:40:02 -0400

    Run make auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,7 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
-
-        <script src="{{ asset('js/app.js') }}"></script>
-    </head>
-    <body>
-        <div id="app">
-            <todo-list></todo-list>
-            <example></example>
-            Hello
-        </div>
-    </body>
-</html>
+@section('content')
+    <div class="container">
+        <todo-list></todo-list>
+    </div>
+@endsection
