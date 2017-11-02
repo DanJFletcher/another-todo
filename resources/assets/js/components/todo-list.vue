dfefffc80e4da5b033c3e102a3a79f730359cfdf
Dan Fletcher <dannyjfletcher@outlook.com>
2017-11-01 21:40:35 -0400

    Work out what component will look like (roughly)

--- resources/assets/js/components/todo-list.vue
+++ resources/assets/js/components/todo-list.vue
@@ -1,11 +1,35 @@
 <template>
-    <div>
-        <ul>
-            <li>List Item</li>
+    <div id="todo-list" class="center-block">
+        <div class="panel panel-default">
+        <!-- Default panel contents -->
+        <div class="panel-heading">Todo List</div>
+        <div class="panel-body">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Something to do...">
+                <span class="input-group-btn">
+                    <button class="btn btn-primary" type="button">Add</button>
+                </span>
+            </div><!-- /input-group -->
+        </div>
+
+        <!-- List group -->
+        <ul class="list-group">
+            <li class="list-group-item">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Morbi leo risus</li>
+            <li class="list-group-item">Porta ac consectetur ac</li>
+            <li class="list-group-item">Vestibulum at eros</li>
         </ul>
+        </div>
     </div>
 </template>
 
 <script>
     export default {}
 </script>
+
+<style>
+    #todo-list {
+        max-width: 400px;
+    }
+</style>
