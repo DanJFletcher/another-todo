7abea6902a48a77f87801c715d88df289002a638
Dan Fletcher <dannyjfletcher@outlook.com>
2017-11-03 11:39:37 -0400

    Add missing div

--- resources/assets/js/components/todo-list.vue
+++ resources/assets/js/components/todo-list.vue
@@ -2,30 +2,31 @@
     <div id="todo-list" class="center-block">
         <div class="panel panel-default">
         <!-- Default panel contents -->
-        <div class="panel-heading">Todo List</div>
-        <div class="panel-body">
-            <div class="input-group">
-                <input 
-                    type="text" 
-                    class="form-control" 
-                    placeholder="Something to do..."
-                    v-model="text"
-                    @keyup.enter="addItem">
-                <span class="input-group-btn">
-                    <button 
-                        class="btn btn-primary" 
-                        type="button" 
-                        @click="addItem">Add</button>
-                </span>
-            </div><!-- /input-group -->
-        </div>
+            <div class="panel-heading">Todo List</div>
+                <div class="panel-body">
+                    <div class="input-group">
+                        <input 
+                            type="text" 
+                            class="form-control" 
+                            placeholder="Something to do..."
+                            v-model="text"
+                            @keyup.enter="addItem">
+                        <span class="input-group-btn">
+                            <button 
+                                class="btn btn-primary" 
+                                type="button" 
+                                @click="addItem">Add</button>
+                        </span>
+                    </div><!-- /input-group -->
+                </div>
 
-        <!-- List group -->
-        <ul class="list-group">
-            <li class="list-group-item" v-for="listItem in listItems" :key="listItem.id">
-                <list-item :text="listItem.text" :id="listItem.id"></list-item>
-            </li>
-        </ul>
+                <!-- List group -->
+                <ul class="list-group">
+                    <li class="list-group-item" v-for="listItem in listItems" :key="listItem.id">
+                        <list-item :text="listItem.text" :id="listItem.id"></list-item>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
